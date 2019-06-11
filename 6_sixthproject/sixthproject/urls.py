b7dde7ca7a9af8f772f06edfb850663d33c149e8
merry-Hyelyn <h1335@naver.com>
2019-06-11 15:31:35 +0900

    word count

--- 6_sixthproject/sixthproject/urls.py
+++ 6_sixthproject/sixthproject/urls.py
@@ -20,4 +20,6 @@ import wordcount.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wordcount.views.home, name='home'),
+    path('about/', wordcount.views.about, name='about'),
+    path('result/', wordcount.views.result, name='result'),
 ]
