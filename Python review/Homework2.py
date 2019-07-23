8b65b44ed0bce4db80c57699201415b826542a74
merry-Hyelyn <h1335@naver.com>
2019-07-24 01:28:50 +0900

    Homework Re

--- Python review/Homework2.py
+++ Python review/Homework2.py
@@ -1,33 +1,35 @@
-class Class():
+class Class:
+    def __init__(self,student):
+        self.student = student
 
-    student={}
+    def grade_personal(self,name):
+        return self.student[name]
 
-    def __init__(self, key, value):
-        self.student[key]=value
+    def aver(self):
+        aver = 0
+        for grade in self.student.values():
+            aver = aver + grade
 
-def print_menu():
+        aver = aver/len(self.student)
+        return round(aver,2)
+
+def print_menu(student):
     print("1. 학생개인성적조회","2. 학급전체평균성적 조회", "3. 종료",sep='\n')
 
-def choose_menu():
     while(True):
         num=int(input("메뉴를 선택하세요 : "))
 
         if num==1:
             name = input('성적조회를 원하는 학생의 이름을 입력하세요 : ')
-            print(name,"학생의 성적은",student[name],'점 입니다')
+            print(name,"학생의 성적은",student.grade_personal(name),'점 입니다')
 
         elif num==2:
-            aver = 0
-            for grade in student.values():
-                aver = aver + grade
-
-            print("학급전체평균성적은 ",aver/len(student), "점 입니다.")
+            print("학급전체평균성적은 ",student.aver(), "점 입니다.")
 
         else:
             break
 
-s1 = Class("gPfls",100)
-s2 = Class("jannabi",90)
-s3 = Class("bambi",60)
+stu = {"hl":100, "jannabi" : 90, "bambi" : 60}
+s1 = Class(stu)
 
-print_menu
+print_menu(s1)
